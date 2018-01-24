9403cab450305600609747ab1dc64f171cf54a9f
Claire Campbell <cc@Claires-MacBook-Pro.local>
2018-01-24 19:26:55 +1300

    creating the new components

---
+++ src/UserInput/UserInput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserInput = (props) => {
+    return (
+        <div>
+            <input type="submit" value="Submit"></input>
+        </div>
+    )
+}
+
+export default UserInput;
---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const UserOutput = (props) => {
+    return (
+        <div>
+            <p>Username 1: {props.username.name}</p>
+            <p>Username 2: Matt</p>
+        </div>
+    )
+}
+
+export default UserOutput;
