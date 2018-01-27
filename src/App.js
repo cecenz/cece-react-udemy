9a11dc1b19e50ce067d1f1d3b44bf3eaa873ee9b
Claire Campbell <cc@springlaod.co.nz>
2018-01-28 09:46:54 +1300

    creating dynamic list that can be removed or changed on the fly

--- src/App.js
+++ src/App.js
@@ -3,55 +3,82 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
-  state = {
-    persons: [
-      { name: 'Maximillian', age: 28 },
-      { name: 'CC', age: 30 },
-      { name: 'Stephanie', age: 22 }
-    ]
-  };
+    state = {
+        persons: [
+            { id: 'agesg', name: 'Maximillian', age: 28 },
+            { id: 'afgaeg', name: 'CC', age: 30 },
+            { id: 'sethaeha', name: 'Stephanie', age: 22 }
+        ],
+        showPersons: false
+    };
 
-  switchNameHandler = (newName) => {
-    // console.log('was clicked');
-    // DON'T DO THIS - this.state.persons[0].name = 'cc';
-    this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: 'CC', age: 30 },
-        { name: 'Stephanie', age: 22 }
-      ]
-    })
-  }
+    deletePersonHandler = (personIndex) => {
+        /* Without the slice, we are manipulating the original
+        source of truth - not good practice, so using slice
+        will create a copy you can update. */
 
-  changeNameHander = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Maximillian', age: 28 },
-        { name: event.target.value, age: 30 },
-        { name: 'Stephanie', age: 22 }
-      ]
-    })
-  }
+        // const persons = this.state.persons.slice();
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello</h1>
-        <button onClick={() => this.switchNameHandler()}>Switch name</button>
-        <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age} />
-        <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age} 
-          changeName={this.changeNameHander}/>
-        <Person 
-          name={this.state.persons[2].name} 
-          age={this.state.persons[2].age}
-          click={this.switchNameHandler.bind(this, 'bae')} />
-      </div>
-    );
-  }
+        /* OR
+        you can use the spread operator
+        */
+        const persons = [...this.state.persons];
+
+        persons.splice(personIndex, 1);
+        this.setState({persons: persons});
+    }
+
+    changeNameHander = (event, id) => {
+        // Locate the person that we want to update
+        const personIndex = this.state.persons.findIndex(p => {
+            return p.id === id;
+        })
+        // Manipulating the current state:
+        // const person = this.state.persons[personIndex];
+
+        // Creating a 'copy' of the particular person we want to manipulate
+        const person = {
+            ...this.state.persons[personIndex]
+        };
+        person.name = event.target.value;
+        // Create a copy of the persons object
+        const persons = [...this.state.persons];
+        // Insert our new manipulated person name
+        persons[personIndex] = person;
+        // Return the new object with the new name
+        this.setState( {persons: persons} );
+    }
+
+    togglePersonHandler = () => {
+        const doesShow = this.state.showPersons;
+        this.setState({showPersons: !doesShow});
+    }
+
+    render() {
+
+        let persons = null;
+        if (this.state.showPersons) {
+            persons = (
+                <div>
+                {this.state.persons.map((person, index) => {
+                    return <Person 
+                    click={() => this.deletePersonHandler(index)}
+                    name={person.name} 
+                    age={person.age}
+                    key={person.id}
+                    change={(event) => this.changeNameHander(event, person.id)} />
+                })}
+                </div> 
+            );
+        }
+        return (
+            <div className="App">
+                <h1>Hello</h1>
+                <button onClick={this.togglePersonHandler}>Toggle Persons</button>
+                {persons}
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -6,7 +6,7 @@ const person = (props) => {
         <div className='person'>
             <p onClick={props.click}>I am {props.name} and I am {props.age} years old</p>
             <p>{props.children}</p>
-            <input type="text" onChange={props.changeName} value={props.name}  />
+            <input type="text" onChange={props.change} value={props.name}  />
         </div>
     )
 }
